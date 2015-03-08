ad8cdc5b348bfc40535c8d8d95c6ea4f0c284a3f
Josef Ryzí  [Homeoffice] <mrcandiess@gmail.com>
2015-03-08 17:25:21 +0100

    Textový chat nahozený - bez ukládaní do databáze

---
+++ www/js/controllers/chat.js
@@ -0,0 +1,38 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function addToChat(message,  client) {
+	if(client) {
+		client = "ID: "+client;
+	} else {
+		client = "Já";
+	}
+	
+	$("#chatBox").append("<div>"+client+" -- "+message+"</div>");
+	
+}
+
+
+function jquery_receive(msg) {
+	message = msg.msg;
+	client = msg.client_id;
+	
+	addToChat(message, client);
+}
+
+$(function () {
+	myScope = angular.element($("#msgCtrl")).scope();
+
+	$('#message').change(function () {
+		data = {};
+		data.msg = this.value;
+		cmd = "chat";
+		a = myScope.sendChat(cmd, data);
+
+		addToChat(data.msg);
+		
+		this.value = '';
+	});
+});

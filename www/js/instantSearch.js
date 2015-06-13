387584c37509d3f391ec240867cced881b01027b
David Kuna <gobbl3r.x@gmail.com>
2015-06-13 14:13:33 +0200

    Oprava instant search - Přechod na API v3

--- www/js/instantSearch.js
+++ www/js/instantSearch.js
@@ -8,27 +8,34 @@ angular.module('instant-search', [])
 		  $scope.maxResults = results;
 		};
 
-		$scope.$watch('searchQuery', function (search) {			
-			var url = 'http://gdata.youtube.com/feeds/api/videos';										
-			var responsePromise = $http.jsonp( url, 
-				{ 
-					params : {
-						q: search,
-						v: 2,
-						format: 5,
-						'max-results': $scope.maxResults,
-						alt: "jsonc",
-						callback : "JSON_CALLBACK"
-					}
-				} 
-			);
+		$scope.$watch('searchQuery', function (search) {
+			console.log(search);
+			if (typeof search === 'undefined') {
+				$scope.results = [];
+			} else {
+				var url = 'https://www.googleapis.com/youtube/v3/search';										
+				var responsePromise = $http.jsonp( url, 
+					{
+						params : {
+							q: search,
+							part: 'snippet',
+							key: 'AIzaSyA8rrT_VF7jGNmEyM1pPvCT-6AS_D0xFXk',
+							format: 5,
+							'max-results': $scope.maxResults,
+							alt: "json",
+							callback : "JSON_CALLBACK"
+						}
+					} 
+				);
 
-			responsePromise.success(function(response) {
-				if(response.data.items) {
-					$scope.results = response.data.items;					
-				} else {
-					$scope.results = [];
-				}		
-			});													
+				responsePromise.success(function(response) {
+					if(response.pageInfo && response.items) {
+
+						$scope.results = response.items;		
+					} else {
+						$scope.results = [];
+					}		
+				});
+			}
 		});						
 	});
